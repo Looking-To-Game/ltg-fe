28b64f93cd9fde2868becf4eda4253a584ce37b7
Caleb <caleb.wells1@gmail.com>
2017-06-19 15:37:38 -0700

    scaffolding out the feed population in home view

---
+++ app/view/home/home-controller.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = [
+  '$log',
+  '$rootScope',
+  '$window',
+  '$location',
+  'authService',
+  'feedService',
+  function($log, $rootScope, $window, $location, authService, feedService){
+    this.$onInit = () => {
+      $log.debug('#HomeController()');
+
+      if(!$window.localStorage.token){
+        authService.getToken()
+        .then(
+          () => $location.url('/home'),
+          () => $location.url('/signup')
+        );
+      }
+
+      this.title = 'Looking to Game?';
+      this.feed = [];
+
+      this.fetchFeed = () => {
+        return feedService.fetchFeed()
+        .then(posts => {
+          this.feed = posts;
+        })
+        .catch(err => $log.error(err));
+      };
+
+      this.logout = function(){
+        $log.log('#navbarCtrl.logout');
+        authService.logout()
+        .then(
+          () => $location.url('/')
+        );
+      };
+
+      $rootScope.$on('locationChangeSuccess', this.fetchFeed);
+      $rootScope.$on('newPostCreated', this.fetchFeed);
+      return this.fetchFeed;
+
+    };
+  }];
