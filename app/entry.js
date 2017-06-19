07d0b14571876ecb5a16948cef51f1357f96e45b
ScottBrenden <scott.b.brenden@gmail.com>
2017-06-19 14:18:44 -0700

    update to entry and added auth-service

--- app/entry.js
+++ app/entry.js
@@ -1,3 +1,15 @@
 'use strict';
 
 require('./scss/main.scss');
+
+const path  = require('path');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const angular = require('angular');
+
+require('@uirouter/angularjs');
+
+const ltg = angular.module('ltg', ['ui.router']);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys.forEach(path => ltg.config(context(path)));
