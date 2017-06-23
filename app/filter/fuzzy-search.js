8beb5ffcc5fd5059398bf449674c724378d6e1f6
Caleb <caleb.wells1@gmail.com>
2017-06-22 22:41:04 -0700

    got the filter for a fuzzy search in the search for the game, need 3 more for the rest

--- app/filter/fuzzy-search.js
+++ app/filter/fuzzy-search.js
@@ -1,1 +1,37 @@
 'use strict';
+//
+// module.exports = function() {
+//   return function(feed, property, filter) {
+//     return feed.filter(post => {
+//       return post[property] === filter;
+//     });
+//   };
+// };
+
+module.exports = function(){
+  return function(filteredCollection, searchValue){
+    let regex = fuzzySearch(searchValue);
+
+    return filteredCollection.filter(value => {
+      return regex.test(value.game.toUpperCase());
+    });
+  };
+};
+
+function fuzzySearch(input){
+  if(!input) return /.*/;
+
+  let searchValue = `.*${input.toUpperCase().split('').join('.*')}.*`;
+  return new RegExp(searchValue);
+}
+
+// module.exports = function(){
+//   return function(galleries, searchTerm){
+//     if(!searchTerm) return /.*/;
+//
+//     let pattern = `.*${searchTerm.toUpperCase().split('').join('.*')}.*`;
+//     let regExp = new RegExp(pattern);
+//
+//     return galleries.filter(gallery => regExp.test(gallery.game.toUpperCase()));
+//   };
+// };
